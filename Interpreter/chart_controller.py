b92ea967c0a6ecb6016cb0eadfb7166f60ab586a
SamuelC94 <37163061+SamuelC94@users.noreply.github.com>
2018-03-12 19:31:25 +1300

    Add files via upload

--- Interpreter/chart_controller.py
+++ Interpreter/chart_controller.py
@@ -1,9 +1,11 @@
 import plotly
-from plotly.graph_objs import *
+import plotly.graph_objs as go
 from abc import ABCMeta, abstractmethod
 
 
 class GraphType(metaclass=ABCMeta):
+    def __init__(self):
+        self.name = "Sam"
 
     @abstractmethod
     def draw_graph(self, dictionary, key, key2, graph_title):
@@ -13,25 +15,26 @@ class GraphType(metaclass=ABCMeta):
 class GraphScatter(GraphType):
     def draw_graph(self, dictionary, key, key2, graph_title):
         plotly.offline.plot({
-            "data": [Scatter(x=dictionary[key], y=dictionary[key2])],
-            "layout": Layout(title=graph_title)
-        })
+            "data": [go.Scatter(x=dictionary[key], y=dictionary[key2])],
+            "layout": go.Layout(title=graph_title)
+        }, filename='ScatterGraph.html')
 
 
 class GraphPie(GraphType):
     def draw_graph(self, dictionary, key, key2, graph_title):
         plotly.offline.plot({
-            "data": [Pie(labels=dictionary[key], values=dictionary[key2])],
-            "layout": Layout(title=graph_title)
-        })
+            "data": [go.Pie(labels=dictionary[key], values=dictionary[key2])],
+            "layout": go.Layout(title=graph_title)
+        }, filename='PieGraph.html')
 
 
 class GraphBar(GraphType):
     def draw_graph(self, dictionary, key, key2, graph_title):
         plotly.offline.plot({
-            "data": [Bar(x=dictionary[key], y=dictionary[key2])],
-            "layout": Layout(title=graph_title)
-        })
+            "data": [go.Bar(x=dictionary[key], y=dictionary[key2])],
+            "layout": go.Layout(title=graph_title)
+        }, filename='BarGraph.html')
+
 
 
 sample1 = {"data1": [1, 2, 3, 4, 5, 6, 7, 8, 9], "data2": [9, 8, 7, 6, 5, 4, 3, 2, 1]}
@@ -40,14 +43,40 @@ sample2 = {"data1": ["sam", "james", "wesley", "luo feng", "amit sarkar", "mike 
 my_title = "this is my title"
 
 
-a = GraphScatter()
-b = GraphPie()
-c = GraphBar()
+class ChartController:
+    def __init__(self, one, two, three):
+        self.scatter = one
+        self.pie = two
+        self.bar = three
 
-a.draw_graph(sample1, "data1", "data2", my_title)
-b.draw_graph(sample2, "data1", "data2", my_title)
-c.draw_graph(sample3, "data1", "data2", my_title)
+    # def __init__(self):
+    #     self.scatter = GraphScatter()
+    #     self.pie = GraphPie()
+    #     self.bar = GraphBar()
+#
+    def draw_graph_a(self, dictionary, key, key2, graph_title):
+        GraphScatter.draw_graph(self.scatter, dictionary, key, key2, graph_title)
+
+    def draw_graph_b(self, dictionary, key, key2, graph_title):
+        GraphPie.draw_graph(self.pie, dictionary, key, key2, graph_title)
+
+    def draw_graph_c(self, dictionary, key, key2, graph_title):
+        GraphBar.draw_graph(self.bar, dictionary, key, key2, graph_title)
+
+
+example = ChartController(GraphScatter(), GraphPie(), GraphBar())
+example.draw_graph_a(sample1, "data1", "data2", my_title)
+example.draw_graph_b(sample2, "data1", "data2", my_title)
+example.draw_graph_c(sample3, "data1", "data2", my_title)
+
+#a = GraphScatter()
+#b = GraphPie()
+#c = GraphBar()
+
+#a.draw_graph(sample1, "data1", "data2", my_title)
+
+#b.draw_graph(sample2, "data1", "data2", my_title)
+
+#c.draw_graph(sample3, "data1", "data2", my_title)
 
 # only outputting 2 x bar graph rather than 1x bar,pie,scatter
-
-print("PRINING")
